155c460da0c017ffad547d4fcb11a84004bfe3a4
unknown <rostykvoznyak@gmail.com>
2020-10-22 20:25:51 +0300

    Lesson1Task2

---
+++ Lesson1Task2/Lesson1Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 1993;
+            Console.WriteLine(year);
+
+        }
+    }
+}
